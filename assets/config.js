d76703d85842a79925d0ed95a16278c5731f7ac2
Ilya Karpov <karpov.business@gmail.com>
2020-04-06 19:22:35 +0500

    15 работа

--- assets/config.js
+++ assets/config.js
@@ -6,7 +6,7 @@ module.exports = {
     // Порт который слушает сервер
     PORT: PORT || 3000,
     // Ссылка на базу данных
-    DATABASE: NODE_ENV === 'production' ? MONGODB : 'mongodb://localhost:27017/mestodb',
+    DATABASE: 'mongodb://localhost:27017/mestodb',
     // Секретный ключ в зависимости от сборки
     JWT_SECRET: NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
 };
